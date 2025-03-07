5acf37ac7d7ac930f8b1e224c060efe779d357ee
lslucaso <lslemos59@gmail.com>
2025-03-06 22:19:18 -0300

    Created models for Product and Category

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    ...
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=165)
+    price = models.FloatField()
+    description = models.TextField(max_length=600)
+    stock = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    cover = models.ImageField(
+        upload_to='ecommerce/covers/%Y/%m/%d/', blank=True, default=''
+    )
+    
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None
+    )
+    
+    def __str__(self):
+        return self.name
