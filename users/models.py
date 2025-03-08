f2421aa53da7ceac9a756bdb390d825c4f889e0d
lslucaso <lslemos59@gmail.com>
2025-03-08 18:45:09 -0300

    #

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Account(models.Model):
+    account = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    profile_img = models.ImageField(
+        upload_to='ecommerce/profile/%Y/%m/%d/', blank=True, default=''
+    )
